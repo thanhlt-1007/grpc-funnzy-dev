7d28d8548de4972fa94bf12dfa803e3fe36c7b72
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-03-02 17:15:30 +0700

    Add grpc/server package

---
+++ grpc/server/new_server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func NewServer() *Server {
+	log.Println("server.NewServer()")
+	server := Server{
+		listener:   newListener(),
+		grpcServer: newGrpcServer(),
+	}
+	log.Println()
+
+	return &server
+}
+
+func newListener() net.Listener {
+	log.Println("server.newListener()")
+	listener, err := net.Listen("tcp", "0.0.0.0:9000")
+	if err != nil {
+		log.Fatalf("ERROR %s\n", err.Error())
+	}
+	log.Println("\tlistener")
+	log.Printf("\t\tAdd: %s", listener.Addr().String())
+	log.Printf("\t\tNetwork: %s", listener.Addr().Network())
+	log.Println()
+
+	return listener
+}
+
+func newGrpcServer() *grpc.Server {
+	log.Println("server.newGrpcServer()")
+	grpcServer := grpc.NewServer()
+
+	return grpcServer
+}
